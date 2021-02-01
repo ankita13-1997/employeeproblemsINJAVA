72dc131cdad843573df731665a96efd2fb1f7ed3
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 12:47:24 +0530

    calculating wages for a month

--- EmpWAgeBuildUc1.java
+++ EmpWAgeBuildUc1.java
@@ -5,35 +5,37 @@ public class EmpWAgeBuildUc1 {
 		
 		System.out.println("welcome to wage computation program");
 
-
-              int check = (int) (Math.floor(Math.random()*10)%3);
-        System.out.println(check);
+System.out.println("welcome to wage computation program");
+        int full=1;
+        int part=2;
+        
+        
         int WagePerHour= 20;
-        
+        //int fullDayHour=12;
+       // int partDayHour=8;
         int time =0;
-        switch(check)
-        {
-        case 1:
         
+        for(int i=1 ; i<=20 ; i++) {
+        	int check = (int) (Math.floor(Math.random()*10)%3);
+        	System.out.println(check);
+        if(check==full)
+        {   
         	System.out.println("employee is present in full time");
-        	
-        	 time=time+12;
-        	break;
+        	time=time+12;
+        }
         
-        case 2:
+        else if(check==part)
+        {
+        	System.out.println("employee present is part time");
+        	time=time+8;
+        }
         
-        	System.out.println("employee is present in part time");
-        	
-        	time =time+8;
-        	break;
-        
-        
-        default:
-        
-        	System.out.println("employee is absent");
-        	System.out.println("no salary without work");
-        
-	}
+        else
+        {
+        	System.out.println("employyee is absent");
+        	time=time+0;
+        }
+        }
         int totalsalary=time*WagePerHour;
         System.out.println("salary per day of u is " +totalsalary);
   
