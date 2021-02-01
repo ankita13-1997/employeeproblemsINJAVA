6ceb476a14a6288fb0eb29466f51245f8a0168e7
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 11:59:39 +0530

    employee salary is calculated

--- EmpWAgeBuildUc1.java
+++ EmpWAgeBuildUc1.java
@@ -9,17 +9,21 @@ public class EmpWAgeBuildUc1 {
              int full=1;
              double check = Math.floor(Math.random()*10)%2;
              System.out.println(check);
+              int WagePerHour= 20;
+              int fullDayHour=8;
         
-                 if(check==full)
-                  {
-        	   System.out.println("employee is present");
-                  }
+        if(check==full)
+        {
+        	System.out.println("employee is present");
+        	int salary=WagePerHour*fullDayHour;
+        	System.out.println(salary);
+        }
         
-                   else
-                  {
-        	   System.out.println("employee is absent");
-                  }
-
+        else
+        {
+        	System.out.println("employee is absent");
+        	System.out.println("no salary without work");
+        }
 	}
 
 }
