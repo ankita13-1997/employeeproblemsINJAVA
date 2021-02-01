50a323e256b9d5ba300f8cc531361c4d66fac6d6
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 12:11:17 +0530

    part time employees are also included

--- EmpWAgeBuildUc1.java
+++ EmpWAgeBuildUc1.java
@@ -6,11 +6,13 @@ public class EmpWAgeBuildUc1 {
 		System.out.println("welcome to wage computation program");
 
 
-             int full=1;
-             double check = Math.floor(Math.random()*10)%2;
-             System.out.println(check);
-              int WagePerHour= 20;
-              int fullDayHour=8;
+              int full=1;
+        int part=2;
+        double check = Math.floor(Math.random()*10)%3;
+        System.out.println(check);
+        int WagePerHour= 20;
+        int fullDayHour=12;
+        int partDayHour=8;
         
         if(check==full)
         {
@@ -18,12 +20,19 @@ public class EmpWAgeBuildUc1 {
         	int salary=WagePerHour*fullDayHour;
         	System.out.println(salary);
         }
+        else if(check==part)
+        {
+        	System.out.println("employee is present in part time");
+        	int salary=WagePerHour*partDayHour;
+        	System.out.println(salary);
+        	
+        }
         
         else
         {
         	System.out.println("employee is absent");
         	System.out.println("no salary without work");
         }
-	}
 
+}
 }
