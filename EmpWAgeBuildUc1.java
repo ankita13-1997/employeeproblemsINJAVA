32e8a296352315dd56b3899abc4b58a71f286a88
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 13:16:40 +0530

    calculate wages till a condition of total working hours or days reached

--- EmpWAgeBuildUc1.java
+++ EmpWAgeBuildUc1.java
@@ -8,36 +8,45 @@ public class EmpWAgeBuildUc1 {
 System.out.println("welcome to wage computation program");
         int full=1;
         int part=2;
-        
+       
         
         int WagePerHour= 20;
-        //int fullDayHour=12;
-       // int partDayHour=8;
+       
         int time =0;
+        int days=0;
         
-        for(int i=1 ; i<=20 ; i++) {
-        	int check = (int) (Math.floor(Math.random()*10)%3);
-        	System.out.println(check);
-        if(check==full)
-        {   
-        	System.out.println("employee is present in full time");
-        	time=time+12;
-        }
+        for(int d=0 ; d<=20 ; d++)
+           {
+        	while(time<=100)
+        	{
+        		 int check = (int) (Math.floor(Math.random()*10)%3);
+        	    	System.out.println(check);
+        	    	 if(check==full)
+        	         {   
+        	         	System.out.println("employee is present in full time");
+        	         	time=time+12;
+        	         }
+        	         
+        	         else if(check==part)
+        	         {
+        	         	System.out.println("employee present is part time");
+        	         	time=time+8;
+        	         }
+        	         
+        	         else
+        	         {
+        	         	System.out.println("employyee is absent");
+        	         	time=time+0;
+        	         }
+        		
+        	}
+        	
+           }
         
-        else if(check==part)
-        {
-        	System.out.println("employee present is part time");
-        	time=time+8;
-        }
         
-        else
-        {
-        	System.out.println("employyee is absent");
-        	time=time+0;
-        }
-        }
+       
         int totalsalary=time*WagePerHour;
-        System.out.println("salary per day of u is " +totalsalary);
+        System.out.println("employee total salary is " +totalsalary);
   
 }
 }
